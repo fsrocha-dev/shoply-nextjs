339087d3cec70a3ed787b97ab2bd2dcdda07a1f0
Frank Rocha <contato.frankrocha@gmail.com>
2023-04-04 21:49:24 -0300

    test: fix jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+/** @type {import('jest').Config} */
+
+const config = {
 	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-	setupFilesAfterEnv: ["<rootDir/setupTests.js>"],
+	setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
 	transform: {
 		"^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
 	}
-}
+};
+
+module.exports = config;
