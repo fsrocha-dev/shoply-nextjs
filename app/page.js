ef7e683770bcbd4408e2b906e38fa94572fd921d
Frank Rocha <contato.frankrocha@gmail.com>
2023-04-08 22:49:25 -0300

    chore: change head home page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className="text-3xl font-bold underline">Hello Next!</h1>
+
     </main>
   );
 }
